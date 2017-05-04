183e3c989c0d65458eb70d60cc013d3f8c3c838b
Vasiliy Shubko <flit77@gmail.com>
2017-05-04 23:36:45 +0200

    template updated

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -1,3 +1,33 @@
+<template>
+  <div class="content">
+    <div class="container">
+      <div class="Search__container">
+        <input
+          class="Search__input"
+          @keyup.enter="requestData"
+          placeholder="npm package name"
+          type="search" name="search"
+          v-model="package"
+        >
+        <button class="Search__button" @click="requestData">Find</button>
+      </div>
+      <div class="error-message" v-if="showError">
+
+      </div>
+      <hr>
+      <h1 class="title" v-if="loaded"></h1>
+      <div class="Chart__container" v-if="loaded">
+        <div class="Chart__title">
+          Downloads per Day <span></span>
+          <hr>
+        </div>
+        <div class="Chart__content">
+          <line-chart v-if="loaded" :chart-data="downloads" :chart-labels="labels"></line-chart>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
   import { Line } from 'vue-chartjs'
   export default Line.extend({
